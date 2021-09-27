ccce9d56bba925fbcc3623f9c6d09feb9c9307af
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-09-28 02:03:38 +0300

    update cors options

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,22 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const whitelist = ['https://thirsty-kirch-c8b82a.netlify.app', 'http://localhost:3001', 'https://infochat-production.herokuapp.com'];
   app.enableCors({
-    origin: ['https://thirsty-kirch-c8b82a.netlify.app', 'http://localhost:3001', 'https://infochat-production.herokuapp.com'],
-    methods: ['GET', 'POST'],
+    // origin: ['https://thirsty-kirch-c8b82a.netlify.app', 'http://localhost:3001', 'https://infochat-production.herokuapp.com'],
+    // methods: ['GET', 'POST'],
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3005);
 }
