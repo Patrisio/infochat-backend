a470390c0f45bad4e3e3e2ca620c02d465ea644d
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-09-28 01:11:46 +0300

    add new origin for cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://thirsty-kirch-c8b82a.netlify.app',
+    origin: ['https://thirsty-kirch-c8b82a.netlify.app', 'http://localhost:3001'],
   });
   await app.listen(process.env.PORT || 3005);
 }
