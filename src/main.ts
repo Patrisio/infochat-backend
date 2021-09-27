e467e7bf9cbbb0ac26ebde94b90c357d11349aad
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-09-28 01:38:40 +0300

    add allowed amethods to cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://thirsty-kirch-c8b82a.netlify.app', 'http://localhost:3001'],
+    methods: ['GET', 'POST'],
   });
   await app.listen(process.env.PORT || 3005);
 }
