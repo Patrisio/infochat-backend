47feaab4e2aaf2f2100d2d3c63ad15acac925d30
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-09-28 11:51:34 +0300

    sdf

--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const cors_1 = require("cors");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    console.log('HERE');
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const options = {
+        "origin": "https://thirsty-kirch-c8b82a.netlify.app/",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 200,
+        "credentials": true,
+        "allowedHeaders": "Content-Type, Accept, Authorization",
+    };
+    app.use(cors_1.default(options));
     await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
