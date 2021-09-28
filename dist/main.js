13aea259ea7a913cca2762c05d071c692ead63ab
Alexandr Plotnikov <aplotnikov@wakeapp.com>
2021-09-28 10:56:53 +0300

    dist2

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,10 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const whitelist = ['https://thirsty-kirch-c8b82a.netlify.app', 'https://thirsty-kirch-c8b82a.netlify.app/', 'http://localhost:3001', 'https://infochat-production.herokuapp.com'];
+    console.log(whitelist, 'whitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelistwhitelist');
     app.enableCors({
         origin: function (origin, callback) {
+            console.log(origin, 'originoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginoriginorigin');
             if (!origin || whitelist.indexOf(origin) !== -1) {
                 console.log("allowed cors for:", origin);
                 callback(null, true);
